c4b3299f96df72363de35dc2ef4217fc1a256e63
liujie <liujie045.china@outlook.com>
2018-11-14 14:26:42 +0800

    Handy Commit

--- lib/calculator/Calculation.dart
+++ lib/calculator/Calculation.dart
@@ -1,5 +1,3 @@
-
-
 ///
 /// File Name
 ///
@@ -15,52 +13,95 @@ class Calculation {
   static final String add = "+";
 
   static String delPress(String formulaData) {
-    String subString = formulaData.substring(formulaData.length - 2, formulaData.length - 1);
-    if (subString != rem && subString != div && subString != mul && subString != sub && subString != add) {
-      return formulaData.substring(0, formulaData.length - 1);
-    } else {
-      return formulaData.substring(0, formulaData.length - 3);
+    if (formulaData
+        .trim()
+        .isNotEmpty) {
+      String subString = formulaData.substring(formulaData.length - 2, formulaData.length - 1);
+      if (subString != rem && subString != div && subString != mul && subString != sub && subString != add) {
+        return formulaData.substring(0, formulaData.length - 1);
+      } else {
+        return formulaData.substring(0, formulaData.length - 3);
+      }
     }
   }
 
   static String symbolPress(String formulaData, String symbol) {
-    formulaData += formulaData.substring(formulaData.length - 1, formulaData.length) == "." ? "0" : "";
-    String subString = formulaData.substring(formulaData.length - 2, formulaData.length - 1);
-    if (subString != rem && subString != div && subString != mul && subString != sub && subString != add) {
-      if (symbol == rem) {
-        formulaData += " $symbol ";
-      } else if (symbol == div) {
-        formulaData += " $symbol ";
-      } else if (symbol == mul) {
-        formulaData += " $symbol ";
-      } else if (symbol == sub) {
-        formulaData += " $symbol ";
-      } else if (symbol == add) {
-        formulaData += " $symbol ";
+    if (formulaData
+        .trim()
+        .isNotEmpty) {
+      formulaData += formulaData.substring(formulaData.length - 1, formulaData.length) == "." ? "0" : "";
+      String subString = formulaData.substring(formulaData.length - 2, formulaData.length - 1);
+      if (subString != rem && subString != div && subString != mul && subString != sub && subString != add) {
+        if (symbol == rem) {
+          formulaData += " $symbol ";
+        } else if (symbol == div) {
+          formulaData += " $symbol ";
+        } else if (symbol == mul) {
+          formulaData += " $symbol ";
+        } else if (symbol == sub) {
+          formulaData += " $symbol ";
+        } else if (symbol == add) {
+          formulaData += " $symbol ";
+        }
       }
+      return formulaData;
     }
-    return formulaData;
   }
 
   static bool pointPress(String formulaData) {
-    String cache = formulaData.replaceAll(rem, "#").replaceAll(div, "#").replaceAll(mul, "#").replaceAll(sub, "#").replaceAll(add, "#");
-    List<String> splitString = cache.split("#");
-    if (splitString.length == 0) {
+    if (formulaData
+        .trim()
+        .isEmpty) {
       return true;
     } else {
-      String lastString = splitString[splitString.length - 1];
-      if (lastString
-          .trim()
-          .isEmpty) {
+      String cache = formulaData.replaceAll(rem, "#").replaceAll(div, "#").replaceAll(mul, "#").replaceAll(sub, "#").replaceAll(add, "#");
+      List<String> splitString = cache.split("#");
+      if (splitString.length == 0) {
         return true;
       } else {
-        return lastString.contains(".");
+        String lastString = splitString[splitString.length - 1];
+        if (lastString
+            .trim()
+            .isEmpty) {
+          return true;
+        } else {
+          return lastString.contains(".");
+        }
       }
     }
   }
 
   static String equalPress(String formulaData) {
-    List<String> _numData;
-    List<String> _symData;
+    if (formulaData
+        .trim()
+        .isEmpty) {
+      return "0";
+    } else {
+      List<String> _numData = new List();
+      List<String> _symData = new List();
+      int cacheIndex = 0;
+
+      formulaData = formulaData.replaceAll(" ", "");
+      String lastChart = formulaData.substring(formulaData.length - 1, formulaData.length);
+      if (lastChart == rem || lastChart == div || lastChart == mul || lastChart == sub || lastChart == add) {
+        formulaData = formulaData.substring(0, formulaData.length - 1);
+      }
+
+      for (int i = 0; i < formulaData.length; i++) {
+        String cache = formulaData[i];
+        if (cache == rem || cache == div || cache == mul || cache == sub || cache == add) {
+          _symData.add(cache);
+          _numData.add(formulaData.substring(cacheIndex, i));
+          cacheIndex = i + 1;
+        }
+      }
+      if (cacheIndex < formulaData.length) {
+        _numData.add(formulaData.substring(cacheIndex, formulaData.length));
+      }
+
+      print(_numData);
+      print(_symData);
+      return "0";
+    }
   }
 }
