d980f0e2809a27d2a7b054db31a9839faed3f35a
liujie <liujie045.china@outlook.com>
2018-11-14 10:29:07 +0800

    Handy Commit

--- lib/view/HomePage.dart
+++ lib/view/HomePage.dart
@@ -105,7 +105,7 @@ class _HomePageState extends State<HomePage> {
                       "%",
                       onPress: () {
                         setState(() {
-                          widget._formulaData += " % ";
+                          widget._formulaData = Calculation.symbolPress(widget._formulaData, "%");
                         });
                       },
                       radius: 0,
